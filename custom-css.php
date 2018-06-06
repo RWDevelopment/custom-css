789704ca987b0fd98825c5e1877026babcadb4d9
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-06-06 20:10:14 +0200

    Add files via upload

---
+++ custom-css.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ * Flextype ................
+ *
+ * @author .................
+ * @link ....................
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+use Flextype\Component\{Event\Event,Registry\Registry};
+
+//
+// Shortcode: [css]
+//
+Event::addListener('onShortcodesInitialized', function () {
+	
+    Content::shortcode()->addHandler('css', function(ShortcodeInterface $s) {
+		
+		Registry::set('css', $s->getContent());
+
+		Event::addListener('onThemeHeader', function () {
+			
+		   echo'<style>'.Registry::get('css').'</style>';
+            
+        });
+		    	
+    });
+	
+});
